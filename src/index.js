00ea03114ab4b233149528d53f625a20a93de41e
Aman Murya <m.amanmaurya49@gmail.com>
2022-10-29 23:36:52 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -4,46 +4,24 @@ import Cards from './Cards';
 import './index.css';
 import Sdata from './Sdata.js';
 
-console.log(Sdata[0]);
+// console.log(Sdata[0]);
+function ncard(val) {
+  return (
+    <>
+      <Cards 
+        imgsrc={val.imgsrc}
+        title={val.title}
+        Sname={val.Sname}
+        link={val.link}
+    />
+    </>
+  );
+}
 ReactDOM.render(
   <>
     <h1 className="heading_Style">Top five Netflix Series</h1>
-    <Cards
-      imgsrc={Sdata[0].imgsrc}
-      title={Sdata[0].title}
-      Sname={Sdata[0].Sname}
-      link={Sdata[0].link}
-    />
-    <Cards
-      imgsrc={Sdata[1].imgsrc}
-      title={Sdata[1].title}
-      Sname={Sdata[1].Sname}
-      link={Sdata[1].link}
-    />
-    <Cards
-      imgsrc={Sdata[2].imgsrc}
-      title={Sdata[2].title}
-      Sname={Sdata[2].Sname}
-      link={Sdata[2].link}
-    />
-    <Cards
-      imgsrc={Sdata[3].imgsrc}
-      title={Sdata[3].title}
-      Sname={Sdata[3].Sname}
-      link={Sdata[3].link}
-    />
-    <Cards
-      imgsrc={Sdata[5].imgsrc}
-      title={Sdata[5].title}
-      Sname={Sdata[5].Sname}
-      link={Sdata[5].link}
-    />
-    <Cards
-      imgsrc={Sdata[5].imgsrc}
-      title={Sdata[5].title}
-      Sname={Sdata[5].Sname}
-      link={Sdata[5].link}
-    />
+    
+    {Sdata.map(ncard)}
   </>,
   document.getElementById('root')
 );
